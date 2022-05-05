f706a2ad2522e6bec43bc5b7b219fc4eb92f94f9
Ziv Chang <htsn901010@gmail.com>
2022-05-04 21:25:36 -0500

    add product list

---
+++ product.py
@@ -0,0 +1,15 @@
+products = []
+while True:
+	name = input('enter your product name: ')
+	if name == 'q':
+		break
+	price = input('enter your product price: ')
+	p = []
+	p.append(name)
+	p.append(price)   
+	# 7.8.9 could be:  p = [name, price]  
+	products.append(p)
+	# 7.8.9.11 could be: products.append([name, price])
+print(products)
+
+print(products[0][1])
