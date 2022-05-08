9d88da5b704a7085361b3e66dcf7f0b54838782d
Ziv Chang <htsn901010@gmail.com>
2022-05-08 17:17:53 -0500

    add header

--- product.py
+++ product.py
@@ -16,3 +16,14 @@ print(products[0][1])
 
 for product in products:
 	print('the', product[0], 'price is: ', product[1])
+
+with open('product.csv', 'w', encoding = 'utf-8') as f:
+	f.write('product,price,\n')
+	for product in products:
+		f.write(product[0] + ',' + product[1] + '\n')
+
+# practice
+data = [1, 3, 5, 7, 9] # 清單中裝著一些整數
+with open('test.txt', 'w') as f:
+    for g in data:
+        f.write(str(g) + '\n')
